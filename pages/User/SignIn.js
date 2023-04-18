02db81c6946c7427a4a3a1e0e6583e19b42b7603
Priyanshu Gour <111908314+priyanshugour70@users.noreply.github.com>
2023-04-18 10:54:02 +0530

    Update SignIn.js

--- pages/User/SignIn.js
+++ pages/User/SignIn.js
@@ -4,30 +4,29 @@ import Link from "next/link";
 
 const SignIn = () => {
     return (
-        <section className="h-screen flex flex-col md:flex-row justify-center space-y-10 md:space-y-0 md:space-x-16 items-center my-2 mx-5 md:mx-0 md:my-0">
+        <section className="h-screen flex flex-col md:flex-row justify-center space-y-10 md:space-y-0 md:space-x-16 items-center my-20 mx-5 md:mx-0 md:my-0">
             <div className="md:w-1/3 max-w-sm">
                 <LogIn1 />
             </div>
 
             <div className="md:w-1/3 max-w-sm">
-
-            <div className="my-9 flex justify-center items-center tracking-widest font-normal text-2xl text-gray-600">
-                <div className="flex items-center justify-center w-8 h6 rounded-full dark:bg-violet-400">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 32 32"
-                        fill="currentColor"
-                        className="flex-shrink-0 w-5 h-5 rounded-full dark:text-gray-900"
-                    >
-                        <path d="M18.266 26.068l7.839-7.854 4.469 4.479c1.859 1.859 1.859 4.875 0 6.734l-1.104 1.104c-1.859 1.865-4.875 1.865-6.734 0zM30.563 2.531l-1.109-1.104c-1.859-1.859-4.875-1.859-6.734 0l-6.719 6.734-6.734-6.734c-1.859-1.859-4.875-1.859-6.734 0l-1.104 1.104c-1.859 1.859-1.859 4.875 0 6.734l6.734 6.734-6.734 6.734c-1.859 1.859-1.859 4.875 0 6.734l1.104 1.104c1.859 1.859 4.875 1.859 6.734 0l21.307-21.307c1.859-1.859 1.859-4.875 0-6.734z"></path>
-                    </svg>
+                <div className="my-9 flex justify-center items-center tracking-widest font-normal text-2xl text-gray-600">
+                    <div className="flex items-center justify-center w-8 h6 rounded-full dark:bg-violet-400">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 32 32"
+                            fill="currentColor"
+                            className="flex-shrink-0 w-5 h-5 rounded-full dark:text-gray-900"
+                        >
+                            <path d="M18.266 26.068l7.839-7.854 4.469 4.479c1.859 1.859 1.859 4.875 0 6.734l-1.104 1.104c-1.859 1.865-4.875 1.865-6.734 0zM30.563 2.531l-1.109-1.104c-1.859-1.859-4.875-1.859-6.734 0l-6.719 6.734-6.734-6.734c-1.859-1.859-4.875-1.859-6.734 0l-1.104 1.104c-1.859 1.859-1.859 4.875 0 6.734l6.734 6.734-6.734 6.734c-1.859 1.859-1.859 4.875 0 6.734l1.104 1.104c1.859 1.859 4.875 1.859 6.734 0l21.307-21.307c1.859-1.859 1.859-4.875 0-6.734z"></path>
+                        </svg>
+                    </div>
+                    <span className="text-2xl font-semibold text-blue-600">
+                        Doct
+                    </span>{" "}
+                    In
                 </div>
-                <span className="text-2xl font-semibold text-blue-600">
-                    Doct
-                </span>{" "}
-                In
-            </div>
-                
+
                 <input
                     className="text-sm w-full px-4 py-2 border border-solid border-gray-300 rounded"
                     type="text"
@@ -50,12 +49,12 @@ const SignIn = () => {
                         Forgot Password?
                     </a>
                 </div>
-                <div className="text-center md:text-left">
+                <div className="flex flex-col items-center">
                     <button
                         className="mt-4 bg-blue-600 hover:bg-blue-700 px-4 py-2 text-white uppercase rounded text-xs tracking-wider"
                         type="submit"
                     >
-                        Login
+                        Sign In
                     </button>
                 </div>
                 <div className="my-5 flex items-center before:mt-0.5 before:flex-1 before:border-t before:border-neutral-300 after:mt-0.5 after:flex-1 after:border-t after:border-neutral-300">
@@ -63,33 +62,40 @@ const SignIn = () => {
                         Or
                     </p>
                 </div>
-                <div className="text-center md:text-left">
-                    <label className="mr-1">Sign in with</label>
-                    <button
-                        type="button"
-                        className="mx-1 h-9 w-9 rounded-full bg-blue-600 hover:bg-blue-700 text-white shadow-[0_4px_9px_-4px_#3b71ca]"
-                    >
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="mx-auto h-3.5 w-3.5"
-                            fill="currentColor"
-                            viewBox="0 0 24 24"
-                        >
-                            <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z" />
-                        </svg>
+                <div className="flex flex-col items-center">
+                    <button className="w-full max-w-xs font-bold shadow-sm rounded-lg py-3 bg-indigo-100 text-gray-800 flex items-center justify-center transition-all duration-300 ease-in-out focus:outline-none hover:shadow focus:shadow-sm focus:shadow-outline">
+                        <div className="bg-white p-2 rounded-full">
+                            <svg className="w-4" viewBox="0 0 533.5 544.3">
+                                <path
+                                    d="M533.5 278.4c0-18.5-1.5-37.1-4.7-55.3H272.1v104.8h147c-6.1 33.8-25.7 63.7-54.4 82.7v68h87.7c51.5-47.4 81.1-117.4 81.1-200.2z"
+                                    fill="#4285f4"
+                                />
+                                <path
+                                    d="M272.1 544.3c73.4 0 135.3-24.1 180.4-65.7l-87.7-68c-24.4 16.6-55.9 26-92.6 26-71 0-131.2-47.9-152.8-112.3H28.9v70.1c46.2 91.9 140.3 149.9 243.2 149.9z"
+                                    fill="#34a853"
+                                />
+                                <path
+                                    d="M119.3 324.3c-11.4-33.8-11.4-70.4 0-104.2V150H28.9c-38.6 76.9-38.6 167.5 0 244.4l90.4-70.1z"
+                                    fill="#fbbc04"
+                                />
+                                <path
+                                    d="M272.1 107.7c38.8-.6 76.3 14 104.4 40.8l77.7-77.7C405 24.6 339.7-.8 272.1 0 169.2 0 75.1 58 28.9 150l90.4 70.1c21.5-64.5 81.8-112.4 152.8-112.4z"
+                                    fill="#ea4335"
+                                />
+                            </svg>
+                        </div>
+                        <span className="ml-4">Sign In with Google</span>
                     </button>
-                    <button
-                        type="button"
-                        className="inlne-block mx-1 h-9 w-9 rounded-full bg-blue-600 hover:bg-blue-700 uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca]"
-                    >
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="mx-auto h-3.5 w-3.5"
-                            fill="currentColor"
-                            viewBox="0 0 24 24"
-                        >
-                            <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z" />
-                        </svg>
+                    <button className="w-full max-w-xs font-bold shadow-sm rounded-lg py-3 bg-indigo-100 text-gray-800 flex items-center justify-center transition-all duration-300 ease-in-out focus:outline-none hover:shadow focus:shadow-sm focus:shadow-outline mt-5">
+                        <div className="bg-white p-1 rounded-full">
+                            <svg className="w-6" viewBox="0 0 32 32">
+                                <path
+                                    fillRule="evenodd"
+                                    d="M16 4C9.371 4 4 9.371 4 16c0 5.3 3.438 9.8 8.207 11.387.602.11.82-.258.82-.578 0-.286-.011-1.04-.015-2.04-3.34.723-4.043-1.609-4.043-1.609-.547-1.387-1.332-1.758-1.332-1.758-1.09-.742.082-.726.082-.726 1.203.086 1.836 1.234 1.836 1.234 1.07 1.836 2.808 1.305 3.492 1 .11-.777.422-1.305.762-1.605-2.664-.301-5.465-1.332-5.465-5.93 0-1.313.469-2.383 1.234-3.223-.121-.3-.535-1.523.117-3.175 0 0 1.008-.32 3.301 1.23A11.487 11.487 0 0116 9.805c1.02.004 2.047.136 3.004.402 2.293-1.55 3.297-1.23 3.297-1.23.656 1.652.246 2.875.12 3.175.77.84 1.231 1.91 1.231 3.223 0 4.61-2.804 5.621-5.476 5.922.43.367.812 1.101.812 2.219 0 1.605-.011 2.898-.011 3.293 0 .32.214.695.824.578C24.566 25.797 28 21.3 28 16c0-6.629-5.371-12-12-12z"
+                                />
+                            </svg>
+                        </div>
+                        <span className="ml-4">Sign In with GitHub</span>
                     </button>
                 </div>
                 <div className="mt-9 font-semibold text-sm text-slate-500 text-center md:text-left">
