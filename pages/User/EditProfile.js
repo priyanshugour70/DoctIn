27b4ffbca8ab19fdde9e64b317a585c64052d33c
Priyanshu Gour <priyanshugournotes1@gmail.com>
2023-05-24 12:29:58 +0530

    Fix Bug

--- pages/User/EditProfile.js
+++ pages/User/EditProfile.js
@@ -219,7 +219,7 @@ const EditProfile = () => {
         <div className="border-b border-gray-900/10 pb-12">
           <h2 className="text-base font-semibold leading-7 text-gray-900">Notifications</h2>
           <p className="mt-1 text-sm leading-6 text-gray-600">
-            We'll always let you know about important changes, but you pick what else you want to hear about.
+            We&apos;ll always let you know about important changes, but you pick what else you want to hear about.
           </p>
 
           <div className="mt-10 space-y-10">
